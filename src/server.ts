91201035b3b591d3e56c91efd571ba1ef8751395
Krasyo <doanusho@gmail.com>
2023-07-03 19:24:11 +0100

    started auth

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './router';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
