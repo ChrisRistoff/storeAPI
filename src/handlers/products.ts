3032e313ebb3cf9362836b9ad574b6ba969efc8c
Krasyo <doanusho@gmail.com>
2023-07-10 21:28:06 +0100

    added the rest of the products handlers, added @@unique to the Product model to unique combination and get rid of the errors, made syntax quite different, need to read more into prisma docu

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express"
 import prisma from "../db"
 
+declare module 'express' {
+  export interface Request {
+    user: {
+      id: string
+    }
+  }
+}
+
 
 // get all products of a user
-export const getUserProducts = async (req, res: Response) => { 
+export const getUserProducts = async (req: Request, res: Response) => { 
   const user = await prisma.user.findUnique({
     where: { id: req.user.id },
     include: { products: true },
@@ -18,20 +26,26 @@ export const getAllProducts = async (req: Request, res: Response) => {
 }
 
 // get one product
-export const getProduct = async (req: Request, res: Response) => {
+export const getOne = async (req: Request, res: Response) => {
   const id = req.params.id
-  const product = await prisma.product.findUnique({
-    where: { id: id },
+  const product = await prisma.product.findFirst({
+    where: { id : id },
+    include: { belongsTo: true },
   })
   res.json({data: product})
 }
 
 // delete a product
 export const deleteProduct = async (req: Request, res: Response) => {
-  const id = req.params.id
   try {
     const product = await prisma.product.delete({
-      where: { id: id },
+      where: {
+        id: req.params.id,
+        id_belongsToId: {
+          id: req.params.id,
+          belongsToId: req.user.id
+        }
+      },
     })
     res.json({data: product, message: "Product deleted successfully"})
   }
@@ -42,15 +56,33 @@ export const deleteProduct = async (req: Request, res: Response) => {
 
 // update a product
 export const updateProduct = async (req: Request, res: Response) => {
-  const id = req.params.id
   const { name, description } = req.body
   try {
     const product = await prisma.product.update({
-      where: { id: id },
+      where: {
+        id: req.params.id,
+        id_belongsToId: {
+          id: req.params.id,
+          belongsToId: req.user.id
+        }
+      },
       data: { name, description },
     })
     res.json({data: product, message: "Product updated successfully"})
   } catch (error) {
     res.status(500).json({error: error})
   }
+}
+
+// create a new product
+export const createProduct = async (req: Request, res: Response) => {
+  const product = await prisma.product.create({
+    data: {
+      name: req.body.name,
+      description: req.body.description,
+      belongsToId: req.user.id,
+    },
+  })
+
+  res.json({data: product, message: "Product created successfully"})
 }
--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,16 @@ import { createNewUser, signIn } from './handlers/user';
 
 const app = express();
 
+// middleware to allow cross-origin requests from the frontend app
 app.use(cors());
+// middleware to parse json data from the request body into an object
 app.use(express.json());
+// turns params into an object that can be used in the req
 app.use(express.urlencoded({ extended: false }));
+// logs requests to the console in dev mode
 app.use(morgan('dev'));
 
+// routes for products are protected by the auth middleware (protect)
 app.use('/api', protect, router);
 
 // User routes
